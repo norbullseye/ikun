9215f2309df4d7cb59b7f77f5b02688eb96a2e9d
norbullseye <wyjhshzy@163.com>
2023-01-08 16:50:45 +0800

    更改

--- js/main.js
+++ js/main.js
@@ -6,33 +6,48 @@ function tishi(str,time) { //提示文字
         $("#grey").fadeOut(); //淡出提示
     }, time); //等待一段时间
 }
-function num(){//将变量转换为数字
-    branch = Number(branch);
-    litchi = Number(litchi);
-    healthy = Number(healthy);
-    onlyBecause = Number(onlyBecause);
-    basketball = Number(basketball); 
+function addendum(lan,tishiv) {//按钮内容，提示内容
+    $("#tabbox1").append(lan);
+    tishi(tishiv,6000);//提示
 }
-function upset(){ //更新
+function upset() { //更新
     var edom = $("#lanqiu")[0];//判定有无按钮"打篮球"
-    if (branch>=30 && !edom) { //追加按钮”打篮球"
-        var lan="<button id='lanqiu' onclick='lanqiu()'title='花费30树枝以购买篮球,一个篮球每秒能为你提供一个树枝'>打篮球</button>";
-        $("#first").after(lan);
-        tishi("点击[打篮球]花费30树枝以购买篮球,一个篮球每秒能为你提供一个树枝",6000);
+    if (branch>=30 && !edom) {
+        addendum("<button id='lanqiu' onclick='lanqiu()'title='花费30树枝以购买篮球,一个篮球每秒两能为你提供一个树枝'>打篮球</button>","点击[打篮球]花费30树枝以购买篮球,一个篮球每两秒能为你提供一个树枝");
+    }
+    
+    var edom1 = $("#music")[0];//判定有无按钮"music"
+    if (branch>=150 && !edom1) { //追加按钮"music"
+        addendum("<button id='music' onclick='music()'title='点击[music]花费150树枝以购买music,一个music每两秒能将一个树枝转换为两个荔枝'>music</button>","点击[music]花费150树枝以购买music,一个music每两秒能将一个树枝转换为两个荔枝");
     }
 }
+
 function exercise() {  //练习按钮
     branch += 1;
     litchi += 1;
 }
 function besideVillage() { //探索村边按钮
-    
+    var num = Math.floor(Math.random()*100+1)
+    if (num <= 20) { //20%的概率
+        explore += 2;//+2
+    } else {
+        explore += 1;//否则+1
+    }
 }
 function lanqiu() { //打篮球按钮
     if (branch>=30) {
         branch -= 30; //购买一个篮球
         basketball ++; //篮球+1
         tishi("篮球+1",2000);
+    } else {
+        tishi("树枝不足",2000);
+    }
+}
+function music() { //music按钮
+    if (branch>=150) {
+        branch -= 150; //购买一个music
+        musicValue ++; //music+1
+        tishi("music+1",2000);
     } else {
         tishi("树枝不足",2000);
     }
--- js/self_increasing.js
+++ js/self_increasing.js
@@ -1,11 +1,26 @@
+function num(){//将变量转换为数字
+    branch = Number(branch);
+    litchi = Number(litchi);
+    healthy = Number(healthy);
+    onlyBecause = Number(onlyBecause);
+    basketball = Number(basketball);
+    explore = Number(explore); 
+    musicValue = Number(musicValue); 
+
+}
 var timer1 = setInterval(function() { //每两秒执行一次
     num();
     branch = branch + basketball;//树枝每两秒自增
+    if (branch > 0) {
+        branch = branch - musicValue;
+        litchi = litchi + musicValue * 2;//树枝转换成两倍荔枝
+    } 
 }, 2000);
 
 var timer2 = setInterval(function() { //每十五秒执行一次
     num();
-    var placeOnFile = branch+","+litchi+","+healthy+","+onlyBecause+","+basketball+","+place+","+explore;
-    //变量全部拼接在一起
+    var placeOnFile = branch+","+litchi+","+healthy+","+onlyBecause+","+basketball+","+place+","+explore+","+musicValue;
+                    //树枝    +   荔枝   +   健康     +   只因值       +   篮球数量    +   位置   +   探索    +    music数量  
+                    //变量全部拼接在一起
     setCookie("file",placeOnFile);//存档
 }, 15000);
